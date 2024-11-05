03f9fbde7c56b060358c63a762c904dfa360f554
Marco Lopes <marco.lopes@nexus-ag.de>
2024-11-05 01:35:31 +0100

    fixed

--- WebService/DevOps/SendEmail.ps1
+++ WebService/DevOps/SendEmail.ps1
@@ -1,14 +1,13 @@
 ﻿$EmailTo = 'Marco.Lopes@nexus-ag.de'
 $EmailFrom = "DevOps.Medication@nexus-ag.de"
 $Subject = "Webservice Docs" 
-$filenameAndPath = "_site/toc.pdf"
 $Body = "
 Your webservice has been deployed
 "
 
 $SMTPServer = "exchange.nexus-ag.de" 
 $SMTPMessage = New-Object System.Net.Mail.MailMessage($EmailFrom,$EmailTo,$Subject,$Body)
-$attachment = New-Object System.Net.Mail.Attachment($filenameAndPath)
+$attachment = New-Object System.Net.Mail.Attachment($env:FilenameAndPath)
 $SMTPMessage.Attachments.Add($attachment)
 $SMTPClient = New-Object Net.Mail.SmtpClient($SmtpServer, 25) 
 $SMTPClient.Credentials = New-Object System.Net.NetworkCredential("DevOps.Medication@nexus-ag.de", $env:EmailPassword)
